c5c03fc6c8ca58ad6ceeeef99993ada7228135d2
Jas9991 <62826563+Jas9991@users.noreply.github.com>
2024-05-25 13:13:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -173,8 +173,10 @@ app.post("/delete", function(req,res){
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log("Server is up and running on port 3000.");
 });
 
 
